c010f76cde149fdd303a2bd5e13971950656cf23
许丹琦 <1204253728@qq.com>
2020-06-22 16:34:03 +0800

    console

---
+++ console/demo0.js
@@ -0,0 +1,3 @@
+console.log("Node.js - console.log() 方法");
+
+console.info("Node.js - console.info() 方法");
---
+++ console/demo1.js
@@ -0,0 +1,13 @@
+console.log("%j",{
+  OS: "Windows",
+  Version: "8.0",
+  Language: ["English","Chinese"]
+});
+
+var v_json = {
+  OS:"Windows",
+  Version: "8.0",
+  Language: ["English","Chinese"]
+};
+
+console.log("%j",v_json);
